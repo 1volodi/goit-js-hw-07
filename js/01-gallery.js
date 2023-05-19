7f6fc7c22baa8cb9b7a45835ef10b2659b853df6
Volodivir Kuchirka <kuvolodi@gmail.com>
2023-05-20 00:20:00 +0300

    hw-7-full

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,37 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description})=>
+        `<li class="gallery__item">
+            <a class="gallery__link js-target" href="${original}">
+                <img data-image-description="${description}"
+                class="gallery__image js-target"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}" widht=100%
+                />
+            </a>
+        </li>`
+    );
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+container.addEventListener("click", onClick);
+
+function onClick(evt) {
+    evt.preventDefault();
+    const { target } = evt;
+    if (!target.classList.contains("gallery__image")) {
+      return;
+    }
+    
+const instance = basicLightbox.create(
+    `<img src="${target.dataset.source}" width="800" height="600">`
+   );
+   instance.show();
+};
+
+
+
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,30 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const container = document.querySelector('.gallery');
+const markup = galleryItems.map(({preview, original, description})=>
+        `<li class="gallery__item">
+            <a class="gallery__link js-target" href="${original}">
+                <img data-image-description="${description}"
+                class="gallery__image js-target"
+                src="${preview}"
+                data-source="${original}"
+                alt="${description}" widht=100%
+                />
+            </a>
+        </li>`
+    );
+
+container.insertAdjacentHTML('beforeend', markup.join(''));
+container.addEventListener("click", onClick);
+    
+function onClick(evt) {
+    evt.preventDefault();
+    const { target } = evt;
+    if (!target.classList.contains("gallery__image")) {
+      return;
+    }
+
+const lightbox = new SimpleLightbox('.gallery__link',
+ { captionsData: "alt", captionDelay: "250" })
+}
